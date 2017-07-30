e703f64cde79d00ae8e5aaab6c84636768efea97
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-07-30 19:55:28 +0000

    Android build fix

--- service-build.sh
+++ service-build.sh
@@ -22,6 +22,8 @@ export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 export PATH=$PATH:/opt/gradle/gradle-3.4.1/bin
 
 pushd mobile
+cordova telemetry on
+cordova platform add android
 cordova clean android
 cordova build android
 popd
