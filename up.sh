616b782e37072049f174148097a1b8157c0433c7
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-10-15 20:25:56 +0000

    service draft stack of containers

--- up.sh
+++ up.sh
@@ -11,15 +11,15 @@ then
   rm -rf ./cidd/lb/dist
   mkdir ./cidd/lb/dist
   cp ./cidd/lb/nginx.conf.template ./cidd/lb/dist/nginx.conf
+  # ...
   sed -i "s/%WORKER_PROCESSES%/$LB_STATIC_NUM/" ./cidd/lb/dist/nginx.conf
-
+  # ...
   ALLSERVERS=''
   for i in $(seq 1 $LB_STATIC_NUM)
   do
     ALLSERVERS="$ALLSERVERS\nserver ${COMPOSE_PROJECT_NAME}_${LB_STATIC_NAME}_$i;"
   done
   sed -i "s/%ALLSERVERS%/$ALLSERVERS/" ./cidd/lb/dist/nginx.conf
-
 
   # -----------------------
   # configure static
@@ -33,7 +33,7 @@ then
   #sed -i "s/%XASS_DOMAIN%/$XASS_DOMAIN/" ./cidd/php/$XASS_DOMAIN.conf
   #sed -i "s/%XASS_DOMAIN_EMAIL%/$XASS_DOMAIN_EMAIL/" ./cidd/php/$XASS_DOMAIN.conf
 
-  docker-compose -f attlas.yml scale $LB_STATIC_NAME=$LB_STATIC_NUM
+  docker-compose -f attlas.yml scale $LB_STATIC_NAME=$LB_STATIC_NUM $LB_SERVICE_NAME=$LB_SERVICE_NUM
   docker-compose -f attlas.yml up $1
 
 else
