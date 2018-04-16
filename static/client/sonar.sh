d6a666f2c184559c1be0c5fd365bdd9e3cd429f4
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-04-16 10:27:17 +0300

    SonarQube shell script

--- static/client/sonar.sh
+++ static/client/sonar.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -e
-~/projects/sonar-scanner-3.0.3.778/bin/sonar-scanner -X
+export $(cat ./../../.env | grep -v ^# | xargs)
+envsubst < sonar-project.properties.template > sonar-project.properties
+#~/projects/sonar-scanner-3.0.3.778/bin/sonar-scanner -X
+sonar-scanner
