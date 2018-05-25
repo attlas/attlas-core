e63d22e19f8248c6c79d32d73a79ca94ec89fea7
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-05-25 12:12:48 +0300

    API v1 skeleton

--- services/correlator/src/api/v1/impl.js
+++ services/correlator/src/api/v1/impl.js
@@ -3,30 +3,17 @@ var router = express.Router()
 var notImplRouter = express.Router();
 var goalsRouter = express.Router();
 
-notImplRouter.get('/', function(req, res, next) {
+notImplRouter.route('/').all(function(req, res, next) {
   next(new Error('not implemented'));
 });
 
-// middleware that is specific to this router
-/*
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-})
-*/
-// define the home page route
-router.get('/', function (req, res) {
-  res.send('Birds home page');
-})
-// define the about route
-router.get('/about', function (req, res) {
-  res.send('About birds');
-})
-
+// goals
 goalsRouter.route('/')
+  // get list of all active goals
   .get(function (req, res) {
     res.send('get');
   })
+  // create new goal
   .post(function (req, res) {
     console.log(req.body);
     res.json(req.body);
