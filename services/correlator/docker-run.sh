2901cf187e4a358a0c6083ad10d2231b0285a579
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-05-28 13:48:23 +0000

    services.correlator docker build/run/stop

--- services/correlator/docker-run.sh
+++ services/correlator/docker-run.sh
@@ -2,4 +2,4 @@
 
 export $(cat ./../../.env | grep -v ^# | xargs)
 
-docker run -d --rm --name com.attlas.services.correlator com.attlas.service.correlator
+docker run -p ${SERVICES_CORRELATOR_PORT}:${SERVICES_CORRELATOR_PORT} -d --rm --name com.attlas.services.correlator com.attlas.services.correlator
