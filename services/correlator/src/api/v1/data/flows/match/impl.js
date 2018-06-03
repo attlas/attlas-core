500dc51375a0fe90b62bc13cda00325890c04a6d
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-06-03 09:11:21 +0300

    correlator: basic match calculation

--- services/correlator/src/api/v1/data/flows/match/impl.js
+++ services/correlator/src/api/v1/data/flows/match/impl.js
@@ -1,11 +1,72 @@
-module.exports = function(context, params) {
+"use strict";
+
+module.exports.schema = {
+  "id" : 'matchTagsSchema',
+  "config": {
+    "title": "Match streams of tags",
+    "description": "",
+    "type": "object",
+    "properties": {
+      "streams": {
+        "type": "array",
+        "description": "Text to parse",
+        "minItems": 2,
+        "maxItems": 2,
+        "items": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "string"
+            },
+            "tags": {
+              "type": "object"
+            }
+          },
+          "required": ["id", "tags"]
+        }
+      }
+    },
+    "required": ["streams"]
+  }
+};
+
+module.exports.entry = function(context, params) {
   return new Promise( (resolve, reject) => {
+    const streams = params.streams;
+    const streamsLen = streams.length;
     //
-    context.docs.getDocById('tags/skills.hard.json')
-      .then( (doc) => {
-        resolve(doc.contents());
-      })
-      .catch(e => reject(`Matching algorithm execution error`));
-    //
+    let r = [];
+    for(let i = 0; i < streamsLen; i++){
+      const lKeys = Object.keys(streams[i].tags);
+      for(let j = i+1; j < streamsLen; j++){
+        let v = 0;
+        let dominator = 0;
+        const rKeys = Object.keys(streams[j].tags);
+        //
+        const lV = lKeys.filter(value => -1 !== rKeys.indexOf(value));
+        const rV = rKeys.filter(value => -1 !== lKeys.indexOf(value));
+        //
+        if (lKeys.length) {
+          v += lV.length / lKeys.length;
+          dominator++;
+        }
+        if (rKeys.length) {
+          v += rV.length / rKeys.length;
+          dominator++;
+        }
+        if (dominator) {
+          v /= dominator;
+        }
+        //
+        r.push({
+          left:streams[i].id,
+          right: streams[j].id,
+          leftTags: lV,
+          rightTags: rV,
+          value: v
+        });
+      }
+    }
+    resolve(r);
   });
 }
