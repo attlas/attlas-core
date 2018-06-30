49c46b106a7187a4d8331163445555d60114ef64
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-06-30 21:21:19 +0000

    local env changes

--- static/client/src/environments/consts.ts
+++ static/client/src/environments/consts.ts
@@ -1,7 +1,7 @@
 export const CONSTS = {
   HOSTS: {
-    SELF: 'http://192.168.1.100:8081',
-    BIND_SERVICE: 'http://192.168.1.100:8082'
+    SELF: 'http://46.101.7.84:8080',
+    BIND_SERVICE: 'http://46.101.7.84:8081'
   },
   STORAGE: {
     USER: 'currentUser'
