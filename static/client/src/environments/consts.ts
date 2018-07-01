46e0b111c5e926d48d172dbab302d91e04c39f05
viarmoly <v.iarmolitskyi@gmail.com>
2018-07-01 20:53:45 +0300

    First Com

--- static/client/src/environments/consts.ts
+++ static/client/src/environments/consts.ts
@@ -1,7 +1,7 @@
 export const CONSTS = {
   HOSTS: {
-    SELF: 'http://46.101.7.84:8080',
-    BIND_SERVICE: 'http://46.101.7.84:8081'
+    SELF: 'http://localhost:8080',
+    BIND_SERVICE: 'http://localhost:8081'
   },
   STORAGE: {
     USER: 'currentUser'
