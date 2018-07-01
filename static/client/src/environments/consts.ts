a987b12365c15b60cb1d2c9e354bab960d4db38f
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-07-01 13:28:18 +0300

    static.client mobile build angular-6.0.7

--- static/client/src/environments/consts.ts
+++ static/client/src/environments/consts.ts
@@ -1,7 +1,7 @@
 export const CONSTS = {
   HOSTS: {
-    SELF: 'http://:',
-    BIND_SERVICE: 'http://:'
+    SELF: 'http://192.168.1.191:8080',
+    BIND_SERVICE: 'http://192.168.1.191:8444'
   },
   STORAGE: {
     USER: 'currentUser'
