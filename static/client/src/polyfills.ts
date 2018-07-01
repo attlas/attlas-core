2ec53333210321bee04a58658bfd053e76dc9ca9
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-07-01 12:54:06 +0000

    static.client exclude unut tests for now

--- static/client/src/polyfills.ts
+++ static/client/src/polyfills.ts
@@ -63,5 +63,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'intl';
+// import 'intl';
 
