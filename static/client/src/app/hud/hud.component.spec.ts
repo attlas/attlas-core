ffd6e8fc3dc23bafb88fabdc8326bdc2a02c88b8
viarmoly <v.iarmolitskyi@gmail.com>
2018-07-09 21:07:28 +0300

    Fixed tests

--- static/client/src/app/hud/hud.component.spec.ts
+++ static/client/src/app/hud/hud.component.spec.ts
@@ -1,15 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HudComponent } from './hud.component';
+import {NavbarComponent} from "../ctrls/navbar/navbar.component";
+import {ProgressBarComponent} from "../ctrls/progress-bar/progress-bar.component";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('HudComponent', () => {
+  console.log(ProgressBarComponent);
   let component: HudComponent;
   let fixture: ComponentFixture<HudComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(async(() => {console.log(ProgressBarComponent);
     TestBed.configureTestingModule({
-      declarations: [ HudComponent]
+      declarations: [HudComponent, NavbarComponent, ProgressBarComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
+
     .compileComponents();
   }));
 
