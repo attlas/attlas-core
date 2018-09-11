0ff81e6acfa218bb6d447d37b522da8d65b7eec2
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-09-11 19:04:06 +0000

    docker based working version

--- static/client/.env.sh
+++ static/client/.env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-if [ -f './../../.env' ]; then
-  echo '## Export variables from project .env file'
-  export $(cat ./../../.env | grep -v ^# | xargs)
-fi
-
 if [ -f './.env' ]; then
   echo '## Export variables from local .env file'
   export $(cat ./.env | grep -v ^# | xargs)
